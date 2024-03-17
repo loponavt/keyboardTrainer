5a498ba6548bbcb8244758dc1ea30677e1f7bef1
loponavt <71023024+loponavt@users.noreply.github.com>
2024-03-17 21:25:58 +0300

    little changes

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import requests
 
 
 class MainWindow(QDialog):
-    char_total = 0
-    char_correctly = 0
-    speed = 0
-
-    def __init__(self, parent=None):
+    def __init__(self, text_to_type: str, parent=None):
         self.timer_thread = Thread(target=self.timering, daemon=True)
         super(MainWindow, self).__init__(parent)
+
+        self.char_total = 0
+        self.char_correctly = 0
+        self.speed = 0
 
         self.layout = QGridLayout()
         self.title = 'KeyboardTrainer'
@@ -24,8 +24,9 @@ class MainWindow(QDialog):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.text_to_type = QTextEdit(text_to_type)
-        self.text_to_type.setReadOnly(True)
+        self.text_to_type = text_to_type
+        self.te_text_to_type = QTextEdit(text_to_type)
+        self.te_text_to_type.setReadOnly(True)
 
         self.lb_chars_correctly = QLabel("Правильно введено: ")
         self.lb_speed = QLabel("Скорость ввода:")
@@ -33,18 +34,18 @@ class MainWindow(QDialog):
         self.input_text = QLineEdit()
         self.input_text.setMinimumHeight(100)
 
-        self.layout.addWidget(self.text_to_type, 1, 0, 1, 2)
+        self.layout.addWidget(self.te_text_to_type, 1, 0, 1, 2)
         self.layout.addWidget(self.lb_chars_correctly, 2, 0)
         self.layout.addWidget(self.lb_speed, 2, 1)
         self.layout.addWidget(self.input_text, 3, 0, 1, 2)
         self.setLayout(self.layout)
 
         # шрифт
-        self.font = self.text_to_type.font()
+        self.font = self.te_text_to_type.font()
         self.font.setPointSize(14)
         self.lb_speed.setFont(self.font)
         self.lb_chars_correctly.setFont(self.font)
-        self.text_to_type.setFont(self.font)
+        self.te_text_to_type.setFont(self.font)
         self.input_text.setFont(self.font)
 
         self.flag = False
@@ -64,7 +65,7 @@ class MainWindow(QDialog):
     def cursor_position_changed(self):
         cursor_position = self.input_text.cursorPosition() - 1
         try:
-            if text_to_type[cursor_position] == self.input_text.text()[cursor_position]:
+            if self.text_to_type[cursor_position] == self.input_text.text()[cursor_position]:
                 self.char_correctly += 1
                 self.change_color(cursor_position, "green")
             else:
@@ -83,7 +84,7 @@ class MainWindow(QDialog):
             self.timer_thread.start()
             self.flag = True
 
-        cursor = QTextCursor(self.text_to_type.document())
+        cursor = QTextCursor(self.te_text_to_type.document())
         cursor.setPosition(index)
 
         char_format = QTextCharFormat()
@@ -92,21 +93,26 @@ class MainWindow(QDialog):
         cursor.movePosition(QTextCursor.Right, QTextCursor.KeepAnchor)
         cursor.mergeCharFormat(char_format)
         cursor.setPosition(index)
-        self.text_to_type.setTextCursor(cursor)
+        self.te_text_to_type.setTextCursor(cursor)
         self.char_total += 1
         self.lb_chars_correctly.setText(
             "Правильно введено: " + str(round(self.char_correctly / self.char_total * 100, 0))[:-2] + "%")
 
 
-if __name__ == '__main__':
+def main():
     session = requests.Session()
     base_url = "https://fish-text.ru/get"
     try:
         response = session.get(url=base_url)
-        text_to_type = (response.json()["text"])
-        app = QApplication()
-        window = MainWindow()
-        window.show()
-        sys.exit(app.exec_())
     except requests.exceptions.ConnectionError:
-        print("Нет подключения к тытырнету")
+        print("Нет подключения к интернету")
+        return
+    text_to_type = (response.json()["text"])
+    app = QApplication()
+    window = MainWindow(text_to_type)
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
